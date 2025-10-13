3a525f6a9af93efcd51ac960ba197d76724d82ec
CaMi <jeanpaulcami@live.com>
2025-10-13 19:40:53 +0200

    push

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -30,25 +30,18 @@
 
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container">
-                <a class="navbar-brand" href="{{ route('home') }}">
+                <ul class="navbar-nav mx-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                    <li>
+                            <span class="h4">
+    <a class="navbar-brand" href="{{ route('home') }}">
                     <img alt="Logo" src="{{ asset('assets/media/logos/logo.png') }}"
                          class="tw-h-16  tw-border-primary"/>
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
-                        aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarScroll">
-                    <ul class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-                        <li>
-                            <span class="h4">
-
                             </span>
-                        </li>
+                    </li>
 
-                    </ul>
+                </ul>
 
-                </div>
             </div>
         </nav>
 
