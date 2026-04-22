a736ee34f5452d8224a9d30d9921b6ae262aeff2
CaMi <jeanpaulcami@live.com>
2026-04-22 12:32:33 +0200

    whip

--- resources/views/livewire/sales-report.blade.php
+++ resources/views/livewire/sales-report.blade.php
@@ -64,10 +64,10 @@
                         View
                         <i class="bi bi-file-pdf"></i>
                     </button>--}}
-                    <button type="submit" formaction="{{ route('admin.reports.export-sales') }}" formtarget="_blank" class="btn btn-sm btn-success flex-shrink-0">
-                        Export Excel
+{{--                    <button type="submit" formaction="{{ route('admin.reports.export-sales') }}" formtarget="_blank" class="btn btn-sm btn-success flex-shrink-0">--}}
+{{--                        Export Excel--}}
 {{--                        <i class="bi bi-file-earmark-excel"></i>--}}
-                    </button>
+{{--                    </button>--}}
                 </form>
             </div>
             <!--end::Actions-->
